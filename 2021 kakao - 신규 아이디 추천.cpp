64ba5ddbfa996c42b5fa718fd54b4731f1d9c05b
kim970903 <57982965+kim970903@users.noreply.github.com>
2021-08-27 18:11:23 +0900

    Update 2021 kakao - 신규 아이디 추천.cpp
    풀이 성공. level2,3 구간에서 문제가 있었다. push_back 방식으로 바꾸어 넣어주니 해결되었다.

--- 2021 kakao - 신규 아이디 추천.cpp
+++ 2021 kakao - 신규 아이디 추천.cpp
@@ -6,37 +6,47 @@ using namespace std;
 
 string solution(string new_id) {
     string answer = "";
-    answer = new_id;
+    //answer = new_id;
     //level1
-    transform(answer.begin(),answer.end(),answer.begin(),::tolower);
+    //transform(answer.begin(),answer.end(),answer.begin(),::tolower);
+    for(int i = 0;i<new_id.size();i++){
+        new_id[i] = tolower(new_id[i]);
+    }
     //level2
-    for(int i =0;i<sizeof(answer);i++){
-        if(!isalpha(answer[i])){
+    for(int i =0;i<new_id.size();i++){
+        /*if(!isalpha(answer[i])){
             if(!isdigit(answer[i])){
-                if(!(answer[i] == '-')){
-                    if(!(answer[i] == '_')){
-                        if(!(answer[i] == '.')){
+                if(answer[i] != '-'){
+                    if(answer[i] != '_'){
+                        if(answer[i] != '.'){
                             answer[i] = ' ';
                         }
                     }
                 }
             }
+        }*/
+        if(isalpha(new_id[i]) || isdigit(new_id[i]) || new_id[i] == '-' || new_id[i] == '.' || new_id[i] == '_'){
+            answer.push_back(new_id[i]);
         }
     }
-    answer.erase(remove(answer.begin(),answer.end(),' '),answer.end());
     //level3
-    for(int i = 0;i<sizeof(answer);i++){
-        if(answer[i] == '.'){
-            if(answer[i+1] == '.'){
-                answer[i] = ' ';
-            } 
+    string tmp = answer;
+    answer.clear();
+    for(int i = 0;i<tmp.size();i++){
+        if(tmp[i] == '.'){
+            if(tmp[i+1] == '.'){
+                continue;
+            } else{
+                answer.push_back(tmp[i]);
+            }
+        } else{
+            answer.push_back(tmp[i]);
         }
     }    
-    answer.erase(remove(answer.begin(),answer.end(),' '),answer.end());
     //level4
-    for(int i = 0;i<sizeof(answer);i++){
+    for(int i = 0;i<answer.size();i++){
         if(answer[i] == '.'){
-            if(i == 0 || i == sizeof(answer)-1){
+            if(i == 0 || i == answer.size()-1){
                 answer[i] = ' ';
             } 
         }
@@ -47,19 +57,17 @@ string solution(string new_id) {
         answer.append("a");
     }
     //level6
-    if(answer.size() >= 16){
+    if(answer.size() > 15){
         answer = answer.substr(0,15);
     }
-    if(answer[answer.size()-1] == '.'){
-        answer[answer.size()-1] = ' ';
+    if(answer.back() == '.'){
+        answer.back() = ' ';
     }
     answer.erase(remove(answer.begin(),answer.end(),' '),answer.end());
     //level7
     if(answer.size() <=2){
-        string tmp;
-        tmp.assign(answer,answer.size()-1,1);
-        for(int i =answer.size();i<3;i++){
-            answer.append(tmp);
+        while(answer.size() < 3){
+            answer  = answer + answer.back();
         }
     }
     return answer;
